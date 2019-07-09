20ebd82be70af38f4462fc54dd4b26687499c541
Javier <andresramirezsistemas@gmail.com>
2019-07-08 19:25:33 -0500

    first commit

--- app/src/main/java/co/app/viajero/viajero_seguro/Ver_informacion.java
+++ app/src/main/java/co/app/viajero/viajero_seguro/Ver_informacion.java
@@ -6,11 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Ver_informacion extends AppCompatActivity {
 
+
+    Button buttonr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +49,14 @@ public class Ver_informacion extends AppCompatActivity {
         TextView txt_view3 = (TextView) findViewById(R.id.txt_view3);
         txt_view3.setText(idRequisitos);
 
+        buttonr  = (Button) findViewById(R.id.Regresar);
+        buttonr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();  //Para regresar a la vista anterior
+
+            }
+        });
 
     }
 
